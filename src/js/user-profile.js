6d42a8ee1c949e97f1ea2a79d8edb3cb8e324aa3
haoxingbo <haoxingbo@bytedance.com>
2021-03-19 18:05:19 +0800

    修改和删除请求

--- src/js/user-profile.js
+++ src/js/user-profile.js
@@ -8,7 +8,8 @@ const userProfileVm = new Vue({
         password: null,
         password2: null,
         authCode: null,
-        axiosHandler: null
+        axiosHandler: null,
+        preData: null
     },
     created: function () {
         let loginEmail = $.cookie('login-email');
@@ -37,6 +38,7 @@ const userProfileVm = new Vue({
             userProfileVm.age = response.data.data.age;
             userProfileVm.gender = response.data.data['gender'];
             userProfileVm.phone = response.data.data['phone'];
+            userProfileVm.preData = response.data.data;
         }).catch(error => {
             console.log(error);
         })
@@ -55,11 +57,79 @@ const userProfileVm = new Vue({
             console.log("help")
             if (this.password !== null && this.password !== this.password2) {
                 profileToastVm.showToast(WARN, '两次输入密码不一致')
+                return;
             }
-            //todo:修改用户信息
+            if (!this.checkModified) {
+                profileToastVm.showToast(INFO, '没有修改信息')
+                return;
+            }
+            let body = {
+                name: this.name,
+                email: $.cookie('login-email'),
+                phone: this.phone,
+                gender: this.gender,
+                age: this.age,
+                password: this.password,
+                verify_code: this.authCode
+            }
+            let url = ENV + URL.user.modify;
+            this.axiosHandler({
+                url: url,
+                data: this.getValidBody(body),
+                method: 'post'
+            }).then(rsp => {
+                if (rsp.data.status !== 0) {
+                    profileToastVm.showToast(ERROR, '修改失败: ' + rsp.data.message);
+                    return;
+                }
+                profileToastVm.showToast(INFO, '修改成功');
+            }).catch(err => {
+                profileToastVm.showToast(ERROR, '修改失败');
+            });
         },
         deleteUser: function () {
-            //todo:重复弹窗确认后发送删除用户请求
+            if (this.authCode === null) {
+                profileToastVm.showToast(WARN, '验证码为空')
+                return;
+            }
+            let body = {
+                verify_code: this.authCode,
+                email: $.cookie('login-email')
+            };
+            let url = ENV + URL.user.delete;
+            this.axiosHandler({
+                method: 'post',
+                url: url,
+                data: body
+            }).then(rsp => {
+                if (rsp.data.status !== 0) {
+                    profileToastVm.showToast(ERROR, '删除失败: ' + rsp.data.message);
+                    this.authCode = null;
+                    return;
+                }
+                profileToastVm.showToast(INFO, '删除成功');
+                //todo: 跳转到登录界面
+            }).catch(err => {
+                profileToastVm.showToast(ERROR, '删除失败');
+            });
+        },
+        checkModified: function () {
+            for (let key of this.preData) {
+                if (this.preData[key] !== this[key]) {
+                    return true;
+                }
+            }
+            return false;
+        },
+        getValidBody: function (body) {
+            if (body['gender'] === '男') {
+                body['gender'] = 1;
+            } else if (body['gender'] === '女') {
+                body['gender'] = 2;
+            } else {
+                body['gender'] = 0;
+            }
+            return body;
         }
     }
 });
